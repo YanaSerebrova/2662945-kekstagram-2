176d72d600cd51c863dc2c13118612494a6470b6
Яна <leksa87@inbox.ru>
2026-03-12 18:51:13 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-console.log("JS подключен");
+const Names = [
+'Мария',
+'Юля',
+'Павел',
+'Антон',
+'Катя',
+'Даша'
+];
+const Descriptions = [
+'Вид на горы',
+'Горный пейзаж',
+'Прогулка по лесу',
+'Вид на озеро',
+'Закат',
+'Вечерняя прогулка'
+];
+const Comments = [
+'Всё отлично!',
+'В целом всё неплохо. Но не всё.',
+'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const getRandomElement = (elements) => {
+  return elements[getRandomNumber(0, elements.length - 1)];
+}
+let commentId = 1;
+function createComment() {
+  return {
+    id: commentId++,
+    avatar:
+    message: getRandomElement(Comments),
+    name: getRandomElement(Names),
+  };
+}
+
