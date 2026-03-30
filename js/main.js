afe5887de56737fe4bcdc0487810efc4247e6f6f
Яна <leksa87@inbox.ru>
2026-03-30 16:22:02 +0300

    добавила modula8-task2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import { renderPictures } from './cards.js';
-//import { PHOTOS_COUNT } from './data.js';
-//import { getPhotos } from './photos.js';
+import { PHOTOS_COUNT } from './data.js';
+import { getPhotos } from './photos.js';
 
-//const photos = getPhotos(25);
-renderPictures();
-
+const photos = getPhotos(PHOTOS_COUNT);
+renderPictures(photos);
